c9d9a5ee53211dadb8188baabb27f6602e7a0e9a
PArtusP <pouilletartus@gmail.com>
2022-04-29 16:36:27 +0200

    first

---
+++ cypress/integration/cesear.spec.js
@@ -0,0 +1,19 @@
+
+describe('example to-do app', () => {
+    beforeEach(() => {
+      // Cypress starts out with a blank slate for each test
+      // so we must tell it to visit our website with the `cy.visit()` command.
+      // Since we want to visit the same URL at the start of all our tests,
+      // we include it in our beforeEach function so that it runs before each test
+      cy.visit('../../index.html')
+    })
+
+    it(" dss", ()=> {
+        cy.get('[data-cy=cypher-key]')
+        .clear()
+        .type();
+
+        cy.get('[data-cy=cypher-result]')
+        .should('have.text',"");
+    })
+});
